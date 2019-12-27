9a001d3b52dae9ebb871b00127886e73ad464ac1
Michal Tymejczyk <michal.tymejczyk@gmail.com>
2019-12-27 15:42:39 +0100

    Added Event Delegation

--- www/script.js
+++ www/script.js
@@ -1,12 +1,23 @@
-function isOperator(arg) {
-    return ['+', '-', '*', '/'].indexOf(arg) > -1;
+var Calculator = {
+    arr: ["0"],
+    hasOperator: false
 };
 
-function isDot(arg) {
+var { arr, hasOperator } = Calculator;
+
+function isOperator(arg = '') {
+    return ['+', '-', '*', '/', '='].indexOf(arg) > -1;
+};
+
+function isDot(arg = '') {
     return '.' == arg;
 };
 
-function calculate(left, right, op) {
+function isNumber(arg = 0) {
+    return [...Array(10).keys()].indexOf(arg) > -1;
+};
+
+function calculate(left = 0, right = 0, op = '') {
     switch (op) {
         case '+':
             return left + right;
@@ -19,133 +30,136 @@ function calculate(left, right, op) {
     }
 };
 
-function leftSide(arr, index) {
-    var value = "";
+function leftSide(arr = [""], index = 0) {
+    let value = "";
     for (; !isOperator(arr[index]); index++)
         value += arr[index];
 
     return { left: parseFloat(value), index };
 };
 
-function rightSide(arr, index) {
-    var value = "";
+function rightSide(arr = [""], index = arr.length - 1) {
+    let value = "";
     for (; index <= arr.length - 1; index++)
         value += arr[index];
 
     return { right: parseFloat(value), index };
 };
 
-var Calculator = {
-    arr: ["0"],
-    hasOperator: false
-};
+function operatorEvent(screen, value) {
+    if (value === '=' && !hasOperator) {
+        screen.value = arr.join("");
+        hasOperator = false;
+        return;
+    }
 
-var { arr, hasOperator } = Calculator;
+    if (!hasOperator) {
+        arr.push(value);
+        screen.value = arr.join("");
+        hasOperator = true;
+        return;
+    }
+
+    var { left, index } = leftSide(arr);
+    var operator = arr[index++];
+    var { right, index } = rightSide(arr, index);
+
+    if (isNaN(left) || isNaN(right)) return;
+
+    var result = calculate(left, right, operator);
+
+    arr = [];
+    arr.push(result);
+    screen.value = arr.join("");
+    hasOperator = false;
+}
+
+function dotEvent(screen) {
+    for (var i = arr.length; i >= 0; i--) {
+        if (isDot(arr[i])) {
+            return;
+        }
+
+        if (isOperator(arr[i])) {
+            if (isNaN(arr[i + 1]))
+                arr.push(0);
+            break;
+        }
+    }
+    arr.push('.');
+    screen.value = arr.join("");
+    return;
+}
+
+function numberEvent(screen, value) {
+    if (arr.length == 1 && arr[0] == 0) {
+        arr.pop();
+    }
+    arr.push(value);
+    screen.value = arr.join("");
+}
+
+
+function cancelOneEvent(screen) {
+    let last = arr[arr.length - 1];
+    if (!isOperator(last)) {
+        arr.pop();
+    } else {
+        arr.pop();
+        hasOperator = false;
+    }
+
+    if (typeof arr !== 'undefined' && arr.length == 0) {
+        arr.push(0);
+    }
+
+    screen.value = arr.join("");
+}
+
+function cancelAllEvent(screen) {
+    console.clear();
+    arr = [];
+    arr.push(0);
+    hasOperator = false;
+    screen.value = arr.join("");
+}
+
 
 window.onload = () => {
-    var buttons = document.getElementsByTagName('button');
     var screen = document.getElementById('screen');
 
-    // init screen
+    document.getElementById('calculator')
+            .addEventListener('click', (event) => {
+            let { target } = event;
+            let { value } = target;
+            if (target && target.id !== 'screen') {
+                console.log("clicked: " + value);
+                if (isOperator(value)) {
+                    operatorEvent(screen, value);
+                    return;
+                }
+
+                if (isDot(value)) {
+                    dotEvent(screen);
+                    return;
+                }
+
+                if (isNumber(Number(value))) {
+                    numberEvent(screen, value);
+                    return;
+                }
+
+                if (value === 'CE') {
+                    cancelOneEvent(screen);
+                    return;
+                }
+
+                if (value === 'C') {
+                    cancelAllEvent(screen);
+                    return;
+                }
+            }
+        });
+
     screen.value = arr.join("");
-
-    // add styling
-    // listener
-    [...buttons].forEach((button) => {
-        button.addEventListener('click', (event) => {
-            const { target } = event;
-            console.log("LISTENER");
-            if (target.classList.contains('operator')) {
-                console.log('OPERATOR: ' + target.value);
-
-                if (target.value === '=' && !hasOperator){
-                    screen.value = arr.join("");
-                    hasOperator = false;
-                    return;
-                }
-
-                if (!hasOperator) {
-                    arr.push(target.value);
-                    screen.value = arr.join("");
-                    hasOperator = true;
-                    return;
-                } else {
-
-                    var { left, index } = leftSide(arr, 0);
-                    var operator = arr[index++];
-                    var { right, index } = rightSide(arr, index);
-
-                    if (isNaN(left) || isNaN(right)) return;
-
-                    var result = calculate(left, right, operator);
-
-                    console.log(`${left}${operator}${right} = ${result}`);
-
-                    arr = [];
-                    arr.push(result);
-                    screen.value = arr.join("");
-                    hasOperator = false;
-                }
-            }
-
-            if (target.classList.contains('number')) {
-                console.log('NUMBER: ' + target.value);
-                if (arr.length == 1 && arr[0] == 0) {
-                    arr.pop();
-                }
-                arr.push(target.value);
-                screen.value = arr.join("");
-            }
-
-            if (target.classList.contains('dot')) {
-                // if number or dot
-                for (var i = arr.length; i >= 0; i--)
-                    if (isDot(arr[i])) {
-                        console.log('DOT NOT ADDED');
-                        return;
-                    } else if (isOperator(arr[i])) {
-                        if (isNaN(arr[i + 1])) {
-                            console.log('ZERO ADDED');
-                            arr.push(0);
-                        }
-
-                        break;
-                    }
-                //arr.forEach(e => console.log(`num: ${e} isnan: ${isNaN(e)}`));
-                arr.push('.');
-                console.log('DOT ADDED');
-                screen.value = arr.join("");
-                return;
-            }
-
-            if (target.classList.contains('cancel-one')) {
-                var last = arr[arr.length - 1];
-                if (!isOperator(last)) {
-                    arr.pop();
-                    console.log("CLEAR NUM/DOT");
-                } else {
-                    arr.pop();
-                    hasOperator = false;
-                    console.log("CLEAR LAST OPERATOR");
-                }
-
-                if (typeof arr !== 'undefined' && arr.length == 0) {
-                    arr.push(0);
-                }
-
-                screen.value = arr.join("");
-            }
-
-            if (target.classList.contains('cancel-all')) {
-                console.clear();
-                console.log("CLEAR ALL");
-                arr = [];
-                arr.push(0);
-                hasOperator = false;
-                screen.value = arr.join("");
-            }
-
-            console.log("******");
-        })
-    });
 }
